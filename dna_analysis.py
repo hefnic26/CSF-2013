e12dc8f83ffd331c02c2082955881aba69d404bb
hefnic26 <hefnic26@evergreen.edu>
2013-11-11 22:36:56 -0800

    changes to dna_analysis

--- dna_analysis.py
+++ dna_analysis.py
@@ -1,5 +1,5 @@
-# Name: ...
-# Evergreen Login: ...
+# Name: Nicholas Hefling
+# Evergreen Login: hefnic26
 # Computer Science Foundations
 # Programming as a Way of Life
 # Homework 3: DNA analysis (Part 1)
@@ -54,6 +54,10 @@ total_count = 0
 # Number of G and C nucleotides seen so far.
 gc_count = 0
 at_count = 0
+g_count = 0
+a_count = 0
+c_count = 0
+t_count = 0
 
 # for each base pair in the string,
 for bp in seq:
@@ -69,10 +73,51 @@ for bp in seq:
         # increment the count of gc
         at_count = at_count + 1
 
+    # if nucleotide is G
+    if bp == 'G':
+    # increment the count of g
+        g_count = g_count + 1
+
+    # if nucleotide is a
+    elif bp == 'A':
+    # increment the count of a
+        a_count = a_count + 1
+
+    # if nucleotide is C
+    elif bp == 'C':
+    # increment the count of a
+        c_count = c_count + 1
+
+    #if nucleotide is T
+    elif bp == 'T':
+    # increment the count of a
+        t_count = t_count + 1
 
 # divide the gc_count by the total_count
 gc_content = float(gc_count) / total_count
 at_content = float(at_count) / total_count
+
+# divide the G, A, T, C counts by respective total counts
+g_content = float(g_count) / total_count
+a_content = float(a_count) / total_count
+t_content = float(t_count) / total_count
+c_content = float(c_count) / total_count
+
 # Print the answer
 print 'GC-content:', gc_content
 print 'AT-content:', at_content
+print 'A Count:', a_count
+print 'T Count:', t_count
+print 'G Count:', g_count
+print 'C Count:', c_count
+print 'Sum Count:', sum_count
+print 'Total Count:', total_count
+print 'Length of sequence:', len(seq)
+print 'AT/GC Ratio:', AT_GC_Ratio
+
+if gc_content > 0.6:
+    print 'CG Classification: This is a high GC content organism'
+elif gc_content < 0.4:
+    print 'CG Classification: This is a low GC content organism'
+else:
+    print 'CG Classification: This is a moderate GC content organism'
